ad799e508273a770a0707bded6e05180baea7867
iqiyi <iqiyi@11111deMac-mini.local>
2022-01-25 15:31:28 +0800

    chapter2-1

--- lesson/chapter2_1/controller/article.go
+++ lesson/chapter2_1/controller/article.go
@@ -2,7 +2,6 @@ package controller
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/jinzhu/copier"
 	"go-web/lesson/chapter2_1/domain"
 	"go-web/lesson/chapter2_1/response"
 	"net/http"
@@ -22,16 +21,18 @@ func (ctr *Article) List(c *gin.Context) {
 		{ID: 2, Title: "标题2", Content: "内容2", ImageURL: "2.jpg", Author: &domain.Author{ID: 2, Name: "作者2", Desc: "介绍2"}},
 	}
 
-	var response []*response.ArticleList
-	copier.Copy(&response, articles)
-	c.JSON(http.StatusOK, gin.H{"data": response})
-
+	//var response []*response.ArticleList
+	//copier.Copy(&response, articles)
+	//var response = new(response.ArticleLists)
+	response := response.ArticleLists{}
+	c.JSON(http.StatusOK, gin.H{"data": response.Map(articles)})
 }
 
 func (ctr *Article) Detail(c *gin.Context) {
 	article := &domain.Article{ID: 1, Title: "标题1", Content: "内容1", ImageURL: "1.jpg"}
 
-	var response response.Article
-	copier.Copy(&response, article)
-	c.JSON(http.StatusOK, gin.H{"data": response})
+	//var response response.ArticleDetail
+	//copier.Copy(&response, article)
+	response := new(response.ArticleDetail)
+	c.JSON(http.StatusOK, gin.H{"data": response.Map(article)})
 }
--- lesson/chapter2_1/response/article.go
+++ lesson/chapter2_1/response/article.go
@@ -1,9 +1,14 @@
 package response
 
+import (
+	"go-web/lesson/chapter2_1/domain"
+	"go-web/lesson/chapter2_1/pkg/mapper"
+)
+
 type Article struct {
 	ID       int    `json:"id"`
 	Title    string `json:"title"`
-	ImageUrl string `json:"imageUrl"`
+	ImageURL string `json:"imageURL"`
 	Content  string `json:"content"`
 }
 
@@ -12,8 +17,24 @@ type ArticleListAuthor struct {
 	Desc string `json:"desc,omitempty" copier:"-"`
 }
 
+type ArticleLists []*ArticleList
+
+func (resp *ArticleLists) Map(articles []*domain.Article) *ArticleLists {
+	mapper.Map(resp, articles)
+	return resp
+}
+
 type ArticleList struct {
 	Article
 	Content           string `json:"content,omitempty" copier:"-"`
 	ArticleListAuthor `json:"author"`
+}
+
+type ArticleDetail struct {
+	Article
+}
+
+func (resp *ArticleDetail) Map(article *domain.Article) *ArticleDetail {
+	mapper.Map(resp, article)
+	return resp
 }
