7afd49ef232868eb412aa7cf2d00b1a0e2bd99c7
iqiyi <iqiyi@11111deMac-mini.local>
2022-01-25 15:44:29 +0800

    chapter3_1

--- lesson/chapter3_1/controller/user.go
+++ lesson/chapter3_1/controller/user.go
@@ -1,9 +1,7 @@
 package controller
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
-	"github.com/jinzhu/copier"
 	"go-web/lesson/chapter3_1/domain"
 	"go-web/lesson/chapter3_1/request"
 	"go-web/lesson/chapter3_1/response"
@@ -19,32 +17,31 @@ func NewUser() *User {
 }
 
 func (ctr *User) Cash(c *gin.Context) {
-	var request request.UserCash
-	if err := c.ShouldBind(&request); err != nil {
+	request := new(request.UserCash)
+	if err := c.ShouldBind(request); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
-	fmt.Println(request.Money.Value())
+	c.JSON(http.StatusOK, gin.H{"data": request})
 }
 
 func (ctr *User) Info(c *gin.Context) {
 	user := &domain.User{ID: 1, Nickname: "用户昵称", HeaderImage: "header.jpg", Money: 12345, CreateTime: time.Now()}
 
-	var response response.User
-	copier.Copy(&response, user)
-	c.JSON(http.StatusOK, gin.H{"data": response})
+	response := new(response.UserInfo)
+	c.JSON(http.StatusOK, gin.H{"data": response.Map(user)})
 }
 
 func (ctr *User) Update(c *gin.Context) {
-	var request request.UserUpdate
-	if err := c.ShouldBind(&request); err != nil {
+	request := new(request.UserUpdate)
+	if err := c.ShouldBind(request); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
 	user := &domain.User{ID: 1, Nickname: "用户昵称", HeaderImage: request.HeaderImage.Name(), Money: 12345}
-	var response response.User
-	copier.Copy(&response, user)
-	c.JSON(http.StatusOK, gin.H{"data": response})
+
+	response := new(response.UserUpdate)
+	c.JSON(http.StatusOK, gin.H{"data": response.Map(user)})
 }
--- lesson/chapter3_1/response/user.go
+++ lesson/chapter3_1/response/user.go
@@ -1,5 +1,10 @@
 package response
 
+import (
+	"go-web/lesson/chapter3_1/domain"
+	"go-web/lesson/chapter3_1/pkg/mapper"
+)
+
 type User struct {
 	ID          int      `json:"id"`
 	Nickname    string   `json:"nickname"`
@@ -7,3 +12,22 @@ type User struct {
 	Money       Money    `json:"money"`
 	CreateTime DateTime `json:"createTime"`
 }
+
+type UserInfo struct {
+	User
+}
+
+func (resp *UserInfo) Map(user *domain.User) *UserInfo {
+	mapper.Map(resp, user)
+	return resp
+}
+
+type UserUpdate struct {
+	User
+}
+
+func (resp *UserUpdate) Map(user *domain.User) *UserUpdate {
+	mapper.Map(resp, user)
+	return resp
+}
+
